a53782bbab2ef1e7d0efc8dc2dce4baf4500138c
Aaron Marisi <amarisi@hotmail.com>
2015-04-08 21:03:42 -0400

    initial project

---
+++ AngularVisualStudioStarter/app/main.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('Main', main);
+
+    function main() {
+        var vm = this;
+        vm.food = 'Chinese food';
+    }
+})();
